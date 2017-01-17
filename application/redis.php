2e854e3e5df10c29095aa42e3a9ca67b300843ce
lixin <lixin1@douyu.tv>
2017-01-17 15:45:51 +0800

    修改redis配置文件格式

--- application/redis.php
+++ application/redis.php
@@ -5,28 +5,26 @@
  * @author lixin1@douyu.tv
  * @date   2017-1-16
  */
-define("REDIS_CONFIG" , [
-    'redis' => [
-        'cluster' => rc('REDIS_CLUSTER', false),
-        'default' => [
-            [
-                'host'     => rc('REDIS_HOST_MASTER'),
-                'port'     => rc('REDIS_PORT_MASTER'),
-                'password' => rc('REDIS_PASSWORD_MASTER'),
-                'alias'    => 'master',
-            ],
-            [
-                'host'     => rc('REDIS_HOST_SLAVE1'),
-                'port'     => rc('REDIS_PORT_SLAVE1'),
-                'password' => rc('REDIS_PASSWORD_SLAVE1'),
-                'alias'    => 'slave1',
-            ],
-            [
-                'host'     => rc('REDIS_HOST_SLAVE2'),
-                'port'     => rc('REDIS_PORT_SLAVE2'),
-                'password' => rc('REDIS_PASSWORD_SLAVE2'),
-                'alias'    => 'slave2',
-            ]
+define("REDIS_CONFIG", [
+    'cluster' => rc('REDIS_CLUSTER', false),
+    'default' => [
+        [
+            'host' => rc('REDIS_HOST_MASTER'),
+            'port' => rc('REDIS_PORT_MASTER'),
+            'password' => rc('REDIS_PASSWORD_MASTER'),
+            'alias' => 'master',
         ],
+        [
+            'host' => rc('REDIS_HOST_SLAVE1'),
+            'port' => rc('REDIS_PORT_SLAVE1'),
+            'password' => rc('REDIS_PASSWORD_SLAVE1'),
+            'alias' => 'slave1',
+        ],
+        [
+            'host' => rc('REDIS_HOST_SLAVE2'),
+            'port' => rc('REDIS_PORT_SLAVE2'),
+            'password' => rc('REDIS_PASSWORD_SLAVE2'),
+            'alias' => 'slave2',
+        ]
     ],
 ]);
